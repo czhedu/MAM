121f036020046d305b365ce0cdf3e2a30920599c
Zhenhua Cai <czhedu@gmail.com>
2011-08-18 18:34:29 +0800

    implement google image fetcher

--- knowledge/synonym_image/word_image_crawl/word_image_crawl/spiders/chinese_word_image_spider.py
+++ knowledge/synonym_image/word_image_crawl/word_image_crawl/spiders/chinese_word_image_spider.py
@@ -1,35 +1,45 @@
+#
+# Fetch images from google search engine 
+#
+# @author Zhenhua Cai <czhedu@gmail.com>
+# @date   2011-08-18
+# 
+# Below is an example of image link in google server
+#     data-src="http://t3.gstatic.com/images?q=tbn:ANd9GcQzA4FrQnpyiUbdTXzXpxCrzXSYjcq29Ds8c6MwI9KQV2FmilZH"
+#
+# About google image searching api 
+#     http://www.codeproject.com/KB/IP/google_image_search_api.aspx
+#
+
 import re
 from scrapy import log
 from scrapy.spider import BaseSpider
+from scrapy.http import Request
 
 class ChineseWordImageSpider(BaseSpider):
     def __init__(self):
         BaseSpider.__init__(self)
-#        self.reobj_image = re.compile(r"data-src=\"(http:[^\"\s]+)\"\s+")
-        self.reobj_image = re.compile(r"gstatic.com")
+        self.reobj_image = re.compile(r"http://\S+.gstatic.com[^\"\s]+")
 
     name = "image.google.com_chinese"
     start_urls = [
-        "http://images.google.com/search?tbm=isch&q=girl",
-        "http://images.google.com/search?tbm=isch&q=man",
-        "http://images.google.com/search?tbm=isch&q=woman",
-        "http://images.google.com/search?tbm=isch&q=apple"
+        "http://images.google.com/search?tbm=isch&q=apple&start=0"
+
+#        "http://images.google.com/search?tbm=isch&q=girl",
+#        "http://images.google.com/search?tbm=isch&q=man",
+#        "http://images.google.com/search?tbm=isch&q=woman",
+#        "http://images.google.com/search?tbm=isch&q=apple"
     ]
 
     def parse(self, response):
         if "images.google.com" in response.url:
-#            print response.body
-            query_image_result = self.reobj_image.search(response.body)
-#            query_image_result = self.reobj_image.findall("http://www.gstatic.com")
-            print query_image_result
+            image_link_list = self.reobj_image.findall(response.body)
+
+            for image_link in image_link_list:
+                yield Request(image_link, callback=self.parse)
 
         else:
-            file_name = response.url[response.url.rindex():]
-            print file_name
-#            open(file_name, 'wb').write(response.body)
+            file_name = "data/" + response.url[response.url.rindex(":")+1:]
+            open(file_name, 'wb').write(response.body)
 
-# data-src="http://t3.gstatic.com/images?q=tbn:ANd9GcQzA4FrQnpyiUbdTXzXpxCrzXSYjcq29Ds8c6MwI9KQV2FmilZH"
-
-#        filename = response.url.split("/")[-2]
-#        open(filename, 'wb').write(response.body)
 
